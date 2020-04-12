f1bacd8a0eee90151823b90a139dbc4b52574fb5
ccev <ccev>
2020-04-12 20:59:12 +0200

    make the last commit work (maybe)

--- stop_watcher.py
+++ stop_watcher.py
@@ -83,7 +83,7 @@ if len(full_gym_cache) == 0 or len(empty_gym_cache) == 0:
     except:
         print("Error while doing that. Just skipping it.")
 
-if len(edit_list["portals"]) + len(edit_list["stops"]) + len(edit_list["gyms"]):
+if len(edit_list["portals"]) + len(edit_list["stops"]) + len(edit_list["gyms"]) == 6:
     print("Found empty Edit Cache. Trying to fill it now.")
     edit_list = queries.create_edit_list(empty_edit_list)
 
