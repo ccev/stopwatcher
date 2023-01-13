d9392e85e3c521d3cfd5f4d2380a8e0a0b81ae4e
Bad Wolf <robbie@robbiestuart.com>
2023-01-13 10:08:24 -0400

    added fallbacks for newer config items

--- util/config.py
+++ util/config.py
@@ -8,7 +8,7 @@ class create_config:
 
         self.language = config_file.get("Config", "language").lower()
         self.scraper_wait = config_file.getint("Config", "portal_scraper_interval")
-        self.removed_wait = config_file.getint("Config", "removed_stop_wait")
+        self.removed_wait = config_file.getint("Config", "removed_stop_wait", fallback=3600)
 
         self.use_static_map = config_file.getboolean("Maps", "static_map")
         self.static_provider = config_file.get("Maps", "static_map_provider").lower()
@@ -21,7 +21,7 @@ class create_config:
         self.map_url = config_file.get("Maps", "map_url")
         self.map_provider = config_file.get("Maps", "frontend").lower()
         self.map_name = config_file.get("Maps", "map_name")
-        self.geojson = config_file.getboolean("Maps", "geojson")
+        self.geojson = config_file.getboolean("Maps", "geojson", fallback= False)
 
         self.use_geocoding = config_file.getboolean("Maps", "geocoding")
         self.geocoding_provider = config_file.get("Maps", "geocoding_provider").lower()
